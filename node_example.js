217a1005f267b9b42fde9c1e1b6614685c71a2e6
djerko <43620461+djerko@users.noreply.github.com>
2018-10-24 16:53:44 -0300

    Tarea 5

--- node_example.js
+++ node_example.js
@@ -23,7 +23,55 @@ var server = http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'application/json'})
     response.end(JSON.stringify({result:suma}))
   }
+
+  else if (data[0]==='/multiplica') {
+    var mult = 1
+    var input = data[1].split('&')
+    input.forEach(function(numero) {
+      var splitted = numero.split('=')[1]
+      if(isNumeric(splitted)) {
+        //Ok, it's a number
+        var number = Number(splitted)
+        mult = mult*number
+      } else {
+        //It's not a number, so we show an error message and sends to the client
+        response.writeHead(400, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
+      }
+    })
+    //If everithing was OK, we send code 200 response, using json format
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify({result:mult}))
+    }
+
+    else if (data[0]==='/fibonacci') {
+      var serie=[]
+      serie[0]=0
+      serie[1]=1
+      var input = data[1].split('&')
+      input.forEach(function(numero) {
+        var splitted = numero.split('=')[1]
+        if(isNumeric(splitted)) {
+          //Ok, it's a number
+          var number = Number(splitted)
+          for(var i=2; i<=number;i++){
+            serie[i]=serie[i-1]+serie[i-2]
+          }
+
+        } else {
+          //It's not a number, so we show an error message and sends to the client
+          response.writeHead(400, {'Content-Type': 'application/json'})
+          response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
+        }
+      })
+      //If everithing was OK, we send code 200 response, using json format
+      response.writeHead(200, {'Content-Type': 'application/json'})
+      response.end(JSON.stringify({result:serie}))
+
+    }
 })
+
+
 
 /**
  ** isNumeric returns true if input is a number. This can be done using the oppsite behaviour from isNaN function
